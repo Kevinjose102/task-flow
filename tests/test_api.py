af8197a46f9548206fa3318297472f235d0ab1ef
Kevinjose102 <kevinjose2505@gmail.com>
2026-05-22 15:15:24 +0530

    test for the projects.py endpoints

--- tests/test_api.py
+++ tests/test_api.py
@@ -295,3 +295,146 @@ def test_get_project_tasks():
         body[1]["project_id"]
         == project_id
     )
+
+def test_create_project():
+
+    response, username, password = create_user(
+        client
+    )
+
+    token = login(
+        client,
+        username,
+        password
+    )
+
+    project = create_project(
+        client,
+        token
+    )
+
+    assert project.status_code == 200
+
+    body = project.json()
+
+    assert "id" in body
+
+    assert (
+        body["name"]
+        == "test_project"
+    )
+
+def test_get_projects():
+
+    response, username, password = create_user(
+        client
+    )
+
+    token = login(
+        client,
+        username,
+        password
+    )
+
+    create_project(
+        client,
+        token
+    )
+
+    response = client.get(
+        "/projects",
+        headers={
+            "Authorization":
+            f"Bearer {token}"
+        }
+    )
+
+    assert response.status_code == 200
+
+    assert len(
+        response.json()
+    ) > 0
+
+def test_get_project():
+
+    response, username, password = create_user(
+        client
+    )
+
+    token = login(
+        client,
+        username,
+        password
+    )
+
+    project = create_project(
+        client,
+        token
+    )
+
+    project_id = project.json()["id"]
+
+    response = client.get(
+        f"/projects/{project_id}",
+        headers={
+            "Authorization":
+            f"Bearer {token}"
+        }
+    )
+
+    assert response.status_code == 200
+
+    assert (
+        response.json()["id"]
+        == project_id
+    )
+
+def test_delete_project():
+
+    response, username, password = create_user(
+        client
+    )
+
+    token = login(
+        client,
+        username,
+        password
+    )
+
+    project = create_project(
+        client,
+        token
+    )
+
+    project_id = project.json()["id"]
+
+    create_task(
+        client,
+        token,
+        project_id
+    )
+
+    response = client.delete(
+        f"/projects/{project_id}",
+        headers={
+            "Authorization":
+            f"Bearer {token}"
+        }
+    )
+
+    assert response.status_code == 200
+
+    assert response.json() == {
+        "message":
+        "Project deleted"
+    }
+
+    response = client.get(
+        f"/projects/{project_id}",
+        headers={
+            "Authorization":
+            f"Bearer {token}"
+        }
+    )
+
+    assert response.status_code == 404
