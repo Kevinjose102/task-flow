bef91193f96e9dbe66ba5360bb2d8fb3cab41afb
Kevinjose102 <kevinjose2505@gmail.com>
2026-05-25 14:53:15 +0530

    redis bug fixed

--- redis_client.py
+++ redis_client.py
@@ -1,18 +1,17 @@
 import os
 import redis
 
+
 redis_client = redis.Redis(
     host=os.getenv(
         "REDIS_HOST",
         "redis"
     ),
-
     port=int(
         os.getenv(
             "REDIS_PORT",
             6379
         )
     ),
-
     decode_responses=True
 )
