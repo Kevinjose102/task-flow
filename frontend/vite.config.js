9dea9ac751f24df41d4c308161a0f9f02b657a64
Kevinjose102 <kevinjose2505@gmail.com>
2026-05-29 12:59:51 +0530

    added railway as an allowedhost in the vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-
-        host: true
-
-    }
+    host: true,
+    allowedHosts: [
+      "frontend-production-9a96.up.railway.app"
+    ]
+  }
 })
